6f9ced37dfabc65abeb94c025d7bb0cfff3f55b4
dannrs <danniramdhani@proton.me>
2024-04-28 06:40:17 +0700

    setup prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  bracketSpacing: true,
+  endOfLine: 'lf',
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
