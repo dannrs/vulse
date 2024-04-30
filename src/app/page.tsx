7d53a1d34a5b21169b2072db6bdf354a1be5712b
dannrs <danniramdhani@proton.me>
2024-04-30 18:05:42 +0700

    hero section basic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import { MoveRight } from 'lucide-react';
+import Image from 'next/image';
+import { Badge } from '~/components/ui/badge';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+
 export default function Home() {
   return (
-    <div className=''>
-      <div>Hello world</div>
+    <div className='my-20 flex items-center justify-center '>
+      <div className='flex flex-col items-center justify-center gap-5 text-center'>
+        <Badge variant='transparent' className='cursor-pointer' size='lg'>
+          Vulse beta registration is now open!
+          <MoveRight className='ml-1 mt-0.5 h-4 w-4' />
+        </Badge>
+        <h1 className='text-4xl font-bold md:text-5xl lg:text-6xl'>
+          Quickly share your favorites
+        </h1>
+        <p className='mt-5 max-w-prose text-foreground/80 sm:text-lg'>
+          Get instant access to your personalized Spotify data and share it with
+          your friends
+        </p>
+        <div className='w-1/2 flex gap-4'>
+          <Input placeholder='Enter your email address...' />
+          <Button type='submit'>
+            Join beta
+          </Button>
+        </div>
+        <Image src='/hero.png' width={960} height={720} alt='Hero image' />
+      </div>
     </div>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,17 +6,17 @@ import { ChevronRight } from 'lucide-react';
 export default function Navbar() {
   return (
     <div className='flex justify-center'>
-      <nav className='sticky inset-x-0 top-0 z-40 flex min-h-16 w-full max-w-[68rem] items-center justify-between p-3 backdrop-blur-lg transition-all'>
+      <nav className='sticky inset-x-0 top-0 z-40 flex min-h-16 w-full max-w-5xl items-center justify-between p-3 backdrop-blur-lg transition-all'>
         <Link href='/' className='z-50 flex text-2xl font-bold'>
           Vulse
         </Link>
-        <div className='flex items-center gap-5 bg-red-500'>
+        {/* <div className='flex items-center gap-5 bg-red-500'>
           <Link href='/'>Link1</Link>
           <Link href='/'>Link2</Link>
           <Link href='/'>Link3</Link>
           <Link href='/'>Link4</Link>
           <Link href='/'>Link5</Link>
-        </div>
+        </div> */}
         <div className='hidden items-center space-x-4 sm:flex'>
           <ModeToggle />
           <Button size='xs'>
