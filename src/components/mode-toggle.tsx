9975390687939c70236ef149af59315a04bd42fe
dannrs <danniramdhani@proton.me>
2024-05-01 12:24:51 +0700

    mode toggle adjusment

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,37 +1,67 @@
-'use client'
+'use client';
 
-import { Moon, Sun } from 'lucide-react'
-import { useTheme } from 'next-themes'
+import { Computer, ComputerIcon, Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
 
-
-import { useEffect, useState } from 'react'
-import { Button } from '~/components/ui/button'
+import { useEffect, useState } from 'react';
+import { Button } from '~/components/ui/button';
+import { cn } from '~/lib/utils';
 
 const ModeToggle = () => {
-  const { resolvedTheme, setTheme } = useTheme()
-  const [isMounted, setIsMounted] = useState(false)
+  const { resolvedTheme, setTheme } = useTheme();
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsMounted(true)
-  }, [])
+    setIsMounted(true);
+  }, []);
 
-  if (!isMounted) return null
+  if (!isMounted) return null;
 
-  const toggleTheme = () => {
-    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
-  }
-
+  const toggleTheme = (theme: string) => {
+    setTheme(theme);
+  };
 
   return (
-    <Button variant='ghost' size='xs' onClick={toggleTheme}>
-      {resolvedTheme === 'dark' ? (
-        <Moon className='h-4 w-4' />
-      ) : (
+    <div className='flex rounded-full border p-0.5 gap-1'>
+      <Button
+        variant={resolvedTheme === 'light' ? 'default' : 'ghost'}
+        size='xs'
+        onClick={() => toggleTheme('light')}
+        className={cn(
+          `${resolvedTheme === 'light' ? 'bg-accent hover:bg-accent' : 'bg-transparent'}`,
+          'rounded-full text-foreground'
+        )}
+      >
         <Sun className='h-4 w-4' />
-      )}
-      <span className='sr-only'>Toggle theme</span>
-    </Button>
-  )
-}
+        <span className='sr-only'>Toggle light mode</span>
+      </Button>
+      <Button
+        variant={resolvedTheme === 'system' ? 'default' : 'ghost'}
+        size='xs'
+        onClick={() => toggleTheme('system')}
+        className={cn(
+          `${resolvedTheme === 'system' ? 'bg-accent hover:bg-accent' : 'bg-transparent'}`,
+          'rounded-full text-foreground'
+        )}
+      >
+        <Computer className='h-4 w-4' />
+        <span className='sr-only'>Toggle system theme</span>
+      </Button>
 
-export default ModeToggle
+      <Button
+        variant={resolvedTheme === 'dark' ? 'default' : 'ghost'}
+        size='xs'
+        onClick={() => toggleTheme('system')}
+        className={cn(
+          `${resolvedTheme === 'dark' ? 'bg-accent hover:bg-accent' : 'bg-transparent'}`,
+          'rounded-full text-foreground'
+        )}
+      >
+        <Moon className='h-4 w-4' />
+        <span className='sr-only'>Toggle dark mode</span>
+      </Button>
+    </div>
+  );
+};
+
+export default ModeToggle;
