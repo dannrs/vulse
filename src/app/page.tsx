02c7ffd5e6282007ce01dcadbd4351fd85b9c1f6
dannrs <danniramdhani@proton.me>
2024-05-01 10:46:31 +0700

    finish landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,13 +19,96 @@ export default function Home() {
           Get instant access to your personalized Spotify data and share it with
           your friends
         </p>
-        <div className='w-1/2 flex gap-4'>
+        <div className='z-30 flex w-1/2 gap-4'>
           <Input placeholder='Enter your email address...' />
-          <Button type='submit'>
-            Join beta
-          </Button>
+          <Button type='submit'>Join beta</Button>
         </div>
-        <Image src='/hero.png' width={960} height={720} alt='Hero image' />
+        <div className='flex flex-col items-center'>
+          <Image
+            src='/hero-gradient.png'
+            width={980}
+            height={473}
+            alt='Hero gradient'
+            className='mt-[-5rem]'
+          />
+          <Image
+            src='/hero.png'
+            width={960}
+            height={720}
+            alt='Hero image'
+            className='mt-[-23rem]'
+          />
+        </div>
+        <div className='flex flex-col gap-8'>
+          <div>
+            <h2 className='text-lg font-semibold text-[#CD44D0]'>Features</h2>
+            <p className='text-4xl font-semibold'>
+              Access and share your favourites with ease
+            </p>
+          </div>
+          <div className='flex max-w-5xl items-center'>
+            <Image src='/hero.png' width={630} height={360} alt='Hero image' />
+            <div className='mx-8 flex flex-col items-start space-y-1'>
+              <h3 className='text-lg font-semibold text-[#CD44D0]'>
+                Personalized
+              </h3>
+              <p className='max-w-prose text-left text-xl font-semibold'>
+                Get access to your personalized top tracks and top artists with
+                ease
+              </p>
+              <p className='text-left text-sm text-foreground/80'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+                doloremque culpa a amet laboriosam ut nisi! Tenetur iure,
+                corrupti error accusantium, enim quisquam voluptatem, possimus
+                nemo quod facilis blanditiis eum.
+              </p>
+            </div>
+          </div>
+          <div className='flex max-w-5xl flex-row-reverse items-center'>
+            <Image src='/hero.png' width={630} height={360} alt='Hero image' />
+            <div className='mx-8 flex flex-col items-start space-y-1'>
+              <h3 className='text-lg font-semibold text-[#CD44D0]'>
+                Personalized
+              </h3>
+              <p className='max-w-prose text-left text-xl font-semibold'>
+                Get access to your personalized top tracks and top artists with
+                ease
+              </p>
+              <p className='text-left text-sm text-foreground/80'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+                doloremque culpa a amet laboriosam ut nisi! Tenetur iure,
+                corrupti error accusantium, enim quisquam voluptatem, possimus
+                nemo quod facilis blanditiis eum.
+              </p>
+            </div>
+          </div>
+          <div className='flex max-w-5xl items-center'>
+            <Image src='/hero.png' width={630} height={360} alt='Hero image' />
+            <div className='mx-8 flex flex-col items-start space-y-1'>
+              <h3 className='text-lg font-semibold text-[#CD44D0]'>
+                Personalized
+              </h3>
+              <p className='max-w-prose text-left text-xl font-semibold'>
+                Get access to your personalized top tracks and top artists with
+                ease
+              </p>
+              <p className='text-left text-sm text-foreground/80'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+                doloremque culpa a amet laboriosam ut nisi! Tenetur iure,
+                corrupti error accusantium, enim quisquam voluptatem, possimus
+                nemo quod facilis blanditiis eum.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='h-64 rounded-xl w-full bg-[#CD44D0] flex flex-col items-center justify-center space-y-2 my-10'>
+          <h2 className='text-4xl font-semibold'>Vulse is now in beta</h2>
+          <p className='text-foreground/80'>Be the first to access the Vulse beta!</p>
+          <div className='flex w-1/2 gap-4 pt-4'>
+            <Input placeholder='Enter your email address...'/>
+            <Button type='submit'>Join now</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
