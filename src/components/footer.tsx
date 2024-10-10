39a21bce21b6c004be7d2131390bfca6b7c4e26f
dannrs <danniramdhani@proton.me>
2024-10-10 18:51:30 +0700

    finalize footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,55 +1,27 @@
 import Link from 'next/link';
-import { RiGithubFill, RiTwitterFill, RiDiscordFill } from 'react-icons/ri';
-import ModeToggle from './mode-toggle';
+import { RiGithubFill } from 'react-icons/ri';
 
 export default function Footer() {
   return (
-    <footer className='max-w-68 container flex flex-col items-center'>
-      <div className='grid h-52 w-full grid-cols-4 py-8'>
-        <div className='flex flex-col gap-4 text-3xl font-bold'>
-          <div>Vulse</div>
-          <div className='flex gap-2 sm:gap-4 text-xl'>
-            <Link href='/'>
-              <RiGithubFill />
-            </Link>
-            <Link href='/'>
-              <RiTwitterFill />
-            </Link>
-            <Link href='/'>
-              <RiDiscordFill />
-            </Link>
-          </div>
-        </div>
-        <div className='flex flex-col gap-2'>
-          <h4 className='font-semibold'>Product</h4>
-          <div className='flex flex-col gap-2 text-sm text-foreground/80'>
-            <Link href='/'>Product1</Link>
-            <Link href='/'>Product2</Link>
-            <Link href='/'>Product3</Link>
-          </div>
-        </div>
-        <div className='flex flex-col gap-2'>
-          <h4 className='font-semibold'>Resources</h4>
-          <div className='flex flex-col gap-2 text-sm text-foreground/80'>
-            <Link href='/'>Product1</Link>
-            <Link href='/'>Product2</Link>
-            <Link href='/'>Product3</Link>
-          </div>
-        </div>
-        <div className='flex flex-col gap-2'>
-          <h4 className='font-semibold'>Legal</h4>
-          <div className='flex flex-col gap-2 text-sm text-foreground/80'>
-            <Link href='/'>Product1</Link>
-            <Link href='/'>Product2</Link>
-            <Link href='/'>Product3</Link>
-          </div>
-        </div>
+    <footer className='container mb-8 mt-16 flex max-w-2xl flex-col items-center gap-6 text-center'>
+      <div className='flex space-x-4'>
+        <Link
+          href='https://github.com/dannrs/vulse'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <RiGithubFill className='h-6 w-6 text-foreground/80 transition-colors hover:text-foreground' />
+        </Link>
       </div>
-      <div className='max-w-68 flex  h-24 w-full items-center justify-between'>
+      <p className='max-w-lg text-sm text-foreground/80'>
+        All copyrighted content on Vulse belongs to its respective owners. The
+        data is provided by Spotify AB. Vulse is in no way affiliated with
+        Spotify AB.
+      </p>
+      <div className='flex items-center space-x-2'>
         <p className='text-sm text-foreground/80'>
           © 2024 Vulse. All rights reserved.
         </p>
-        <ModeToggle />
       </div>
     </footer>
   );
